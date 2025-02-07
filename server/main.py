904752e4b1b55becb0cf27919785dcb15ad73c58
SeanBrownn <108001967+SeanBrownn@users.noreply.github.com>
2025-02-07 14:49:52 -0500

    adding prisma, env

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 
 # Run with comamand : uvicorn main:app --reload
 from fastapi import FastAPI
 
 app = FastAPI()
+
+load_dotenv()
 
 @app.get("/")
 async def root():
