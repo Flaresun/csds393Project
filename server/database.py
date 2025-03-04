1d14f902dde9fca1d9192c3bef13207d82145ffc
jamestelzrow <jst94@case.edu>
2025-03-04 13:53:05 -0500

    Allow creating new courses

--- server/database.py
+++ server/database.py
@@ -25,7 +25,6 @@ async def get_user(db_conn_pool, email: str):
                 (email,)
             )
             result = await cur.fetchone()
-            print(result)
             if result is not None:
                 return UserInDB(
                     email = result[1],
@@ -34,12 +33,12 @@ async def get_user(db_conn_pool, email: str):
                     role = result[3],
                 )
 
-async def create_new_user(db_conn_poll, email, password_hash, user_role):
+async def create_new_user(db_conn_pool, email, password_hash, user_role):
     """
     Attempts to create a new user with the given email, password has and user role;
     raises a UserAlreadyExistsException if a user with that email already exists
     """
-    async with db_conn_poll.connection() as conn:
+    async with db_conn_pool.connection() as conn:
         async with conn.cursor() as cur:
             try:
                 await cur.execute(
@@ -51,3 +50,45 @@ async def create_new_user(db_conn_poll, email, password_hash, user_role):
                 )
             except UniqueViolation as exc:
                 raise UserAlreadyExistsException() from exc
+
+class DepartmentDoesNotExistException(Exception):
+    """
+    Raised when a faculty member attempts to create a course in a department that does not exist
+    """
+
+class CourseAlreadyExistsException(Exception):
+    """
+    Raised when a faculty member attempts to create a course that already exists
+    """
+
+async def create_new_course(db_conn_pool, department_code, code, name):
+    """
+    Attempts to create a new course with the specified code and name in the specified department.
+    Raises a DepartmentDoesNotExistException if the specified department does not exist, and a
+    CourseAlreadyExists exception if the course already exists.
+    """
+    async with db_conn_pool.connection() as conn:
+        async with conn.cursor() as cur:
+            try:
+                await cur.execute(
+                    """
+                    WITH department AS (
+                        SELECT id FROM departments WHERE code = %s
+                    )
+                    INSERT INTO courses (department_id, code, name)
+                    SELECT department.id, %s, %s
+                    FROM department
+                    RETURNING id
+                    """,
+                    (department_code.upper(), code.upper(), name)
+                )
+                # If we are trying to create a course that already exists, then the insertion will
+                # raise a UniqueViolation.
+                # If the result is None, then the specified department does not exist and we throw
+                # the corresponding error.
+                # Otherwise, we have successfully created the new course.
+                result = await cur.fetchone()
+                if result is None:
+                    raise DepartmentDoesNotExistException()
+            except UniqueViolation as exc:
+                raise CourseAlreadyExistsException() from exc
