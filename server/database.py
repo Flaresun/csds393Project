7a87daaa1821409945c2bacd93a8348f11e9703b
Seth Omeike <sno18@case.edu>
2025-03-04 00:03:39 -0500

    Added some stuff to frontend

---
+++ server/database.py
@@ -0,0 +1,28 @@
+"""
+Methods for interfacing with the database.
+For now, we have a "dummy" database dictionary in here.
+"""
+
+from model import UserInDB
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": True,
+    },
+}
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
